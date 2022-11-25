7cbecd0b1fe815e19a364bbc4d2a249e4f05867a
BIL-LAB2PC00\Ogrenci <seyydinburhanedin.mtal38@gmail.com>
2022-11-25 11:21:16 +0300

    7. UYGULAMA: İşlem adımlarına göre aritmetiksel operatörlerin kullanıldığı

---
+++ app/src/main/java/com/example/temelkomutlarunite_3/Uyg6.java
@@ -0,0 +1,17 @@
+package com.example.temelkomutlarunite_3;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Uyg6  extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        final int PI = 3;
+        int yariCap = 5;
+        System.out.println("Çevre = " + (2*PI*yariCap));
+
+    }
+}
